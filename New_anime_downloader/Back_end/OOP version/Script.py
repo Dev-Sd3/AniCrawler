1ee5f299c448318daf9df99f81f4f63c8b2611db
Shizu-dl3 <99927470+Shizu-dl3@users.noreply.github.com>
2022-08-30 17:00:36 +0300

    forgot to save

--- New_anime_downloader/Back_end/OOP version/Script.py
+++ New_anime_downloader/Back_end/OOP version/Script.py
@@ -9,19 +9,34 @@ header = {
 }
 
 file_handler = logging.FileHandler('logs.log')
-file_handler.setFormatter(logging.Formatter(
-    '%(asctime)s %(levelname)s %(message)s'))
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 logger.info('---- New session initiated ----')
 
+def get_name(link):
+    name = link.split("/")[-1].replace('-', ' ')
+    if "episode" in name:
+        name = name.replace(re.search(' episode(.*)',name).group(), " ")
+    return name
+
+def get_link(name):
+    name = name.replace(" ", "-")
+    link = "https://gogoanime.ee/"+name
+    return link
+
 class anime:
     
     def __init__(self, text):
 
-        
+        if "https" in text or "www" in text:
+            self.link = text
+            self.name = get_name(text)
+        else:
+            self.name = text
+            self.link = get_link(text)
 
-        self.text = text
+        
 
     
